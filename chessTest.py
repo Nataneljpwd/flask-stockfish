a780635e4da1862352d13980c6b209bf22bee54b
Nataneljpwd <natanelrudyuk@gmail.com>
2023-06-17 17:33:06 +0300

    message bug fixes

--- chessTest.py
+++ chessTest.py
@@ -15,22 +15,27 @@ path = ""
 if platform == "darwin":
     path = os.path.abspath("mac-arm")
 elif platform == "win32":
-    path = os.getcwd() + "/stockfish_15.1_win_x64_avx2/stockfish-windows-2022-x86-64-avx2.exe"
+    path = (
+        os.getcwd()
+        + "/stockfish_15.1_win_x64_avx2/stockfish-windows-2022-x86-64-avx2.exe"
+    )
 else:
-    path = os.getcwd() + "/stockfish_15.1_linux_x64_avx2/stockfish-ubuntu-20.04-x86-64-avx2" 
+    path = (
+        os.getcwd()
+        + "/stockfish_15.1_linux_x64_avx2/stockfish-ubuntu-20.04-x86-64-avx2"
+    )
 
 stockfish = Stockfish(path)
-app.secret_key = 'BAD_SERET_KEY'
+app.secret_key = "BAD_SERET_KEY"
 
 
 @app.route("/ai", methods=["POST"])
 def get_best_move():
-    move = request.data.decode('UTF-8')
+    move = request.data.decode("UTF-8")
     if move is None or move == "":
-        print("ionside")
-        mv = to2DArrayIndex(stockfish.get_best_move()) 
+        mv = to2DArrayIndex(stockfish.get_best_move())
         return make_response(mv, 200)
-    #we convert all the moves to the list 
+    # we convert all the moves to the list
     moves = move.split()
     if moves[-1] == "":
         moves.pop()
@@ -46,44 +51,63 @@ def get_best_move():
     outcome = board.outcome()
     if outcome is not None:
         res = outcome.result()
+        print(res)
         if res == "1/2-1/2":
-            #its a draw
+            # its a draw
             if board.is_stalemate():
-                return jsonify("STALEMATE")
+                print("STALEMATE")
+                return jsonify("STALEMATE+" + to2DArrayIndex(best_move))
             if board.is_insufficient_material():
-                return jsonify("MAT")
+                print("INSUFFICIENT_MATERIAL")
+                return jsonify("MATERIAL+" + to2DArrayIndex(best_move))
             if board.is_fivefold_repetition():
-                return jsonify("REPETITION")
+                print("FIVEFOLD_REP")
+                return jsonify("REPETITION+" + to2DArrayIndex(best_move))
             if board.is_fifty_moves():
-                return jsonify("FIFTY_MOVE")
+                print("FIFTY_MOVE " + best_move)
+                return jsonify("FIFTY_MOVE+" + to2DArrayIndex(best_move))
         elif res == "1-0":
-            #the white won
-            return jsonify("MATE-WHITE")#means the white won
+            # the white won
+            print("WHITE_WON")
+            return jsonify(
+                "MATE-WHITE+" + to2DArrayIndex(best_move)
+            )  # means the white won
         elif res == "0-1":
-            return jsonify("MATE-BLACK")
+            print("BLACK_WON")
+            return jsonify(
+                "MATE-BLACK+" + to2DArrayIndex(best_move)
+            )  # means the black won
     # or get top n moves using stockfish.get_top_moves(3)
     print(stockfish.get_board_visual())
     return jsonify(to2DArrayIndex(best_move))
+
+
 # return jsonify(best_move):w
 
+
 def to2DArrayIndex(algebraicNotation):
-    file1, rank1, file2, rank2 = algebraicNotation[0], algebraicNotation[
-        1], algebraicNotation[2], algebraicNotation[3]
+    file1, rank1, file2, rank2 = (
+        algebraicNotation[0],
+        algebraicNotation[1],
+        algebraicNotation[2],
+        algebraicNotation[3],
+    )
     promotion = ""
-    if(len(algebraicNotation) == 5):
+    if len(algebraicNotation) == 5:
         promotion = algebraicNotation[4]
 
     file1 = string.ascii_lowercase.index(file1)
     file2 = string.ascii_lowercase.index(file2)
 
-    rank1 = int(rank1)-1
-    rank2 = int(rank2)-1
-    rank1, rank2 = 7-rank1, 7-rank2
+    rank1 = int(rank1) - 1
+    rank2 = int(rank2) - 1
+    rank1, rank2 = 7 - rank1, 7 - rank2
 
-    square = f"{rank1},{file1}:{rank2},{file2}" + (f":{promotion}" if len(algebraicNotation)==5 else "")
+    square = f"{rank1},{file1}:{rank2},{file2}" + (
+        f":{promotion}" if len(algebraicNotation) == 5 else ""
+    )
 
     return square
-
 
 
 if __name__ == "__main__":
